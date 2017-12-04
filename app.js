cc283aa684d376b12b9f906bce5b0657979832b8
sungpado <34066247+sungpado@users.noreply.github.com>
2017-12-04 23:26:08 +0900

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ client.on("message", async message => {
 
     message.delete().catch(O_o=>{});
   
-    message.channel.send("https://cdn.discordapp.com/attachments/384356885970812928/385089929539223566/goodmoring0.5s.gif");
+    message.channel.send
+    icon_url: ("https://cdn.discordapp.com/attachments/384356885970812928/385089929539223566/goodmoring0.5s.gif");
   }
   
   
@@ -46,7 +47,8 @@ client.on("message", async message => {
 
     message.delete().catch(O_o=>{});
   
-    message.channel.send("https://cdn.discordapp.com/attachments/384356885970812928/385089929539223562/goodnight0.5s.gif");
+    message.channel.send
+    icon_url: ("https://cdn.discordapp.com/attachments/384356885970812928/385089929539223562/goodnight0.5s.gif");
   
    }
   
