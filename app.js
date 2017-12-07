e4ebf24f06606ad1b6dec203038f9764768ae7ca
sungpado <34066247+sungpado@users.noreply.github.com>
2017-12-07 20:39:31 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const yt = require('ytdl-core');
 const client = new Discord.Client()
 const config = require("./config.json");
 const embed = new Discord.RichEmbed()
+const ce = require("embed-creator");
 
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
