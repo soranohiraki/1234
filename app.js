89ca157cfa1b3be98c752fcee7a633c1dcf3b0a9
sungpado <34066247+sungpado@users.noreply.github.com>
2017-12-07 20:28:23 +0900

    Update app.js

--- app.js
+++ app.js
@@ -55,9 +55,11 @@ client.on("message", async message => {
 
     message.delete().catch(O_o=>{});
   
-    {"thumbnail": msg.guild.iconURL(), "image": ("https://cdn.discordapp.com/attachments/384356885970812928/385089929539223566/goodmoring0.5s.gif");
-  }}, false
-))
+   msg.channel.send(ce(
+  "#FEAFEA", {"thumbnail": msg.guild.iconURL(), "image": ("https://cdn.discordapp.com/attachments/384356885970812928/385089929539223566/goodmoring0.5s.gif");
+  }, false
+    ))
+
   
   
   if(command === "아바타") {
