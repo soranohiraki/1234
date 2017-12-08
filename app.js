c3bd3512f8a014517ddb4d01e79360dd273c84ee
sungpado <34066247+sungpado@users.noreply.github.com>
2017-12-08 17:48:21 +0900

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,24 @@ client.on("guildDelete", guild => {
   client.user.setGame(`호랑이굴입니다!`);
 });
 
+client.on("참가", message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
 
+  if (message.content === '/join') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { // Connection is an instance of VoiceConnection
+          message.reply('I have successfully connected to the channel!');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+  }
+});
 
 client.on("message", async message => {
   if(message.author.bot) return;
