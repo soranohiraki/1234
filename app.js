aebe26c807592907fc9d7dfb37ad29577280217c
sungpado <34066247+sungpado@users.noreply.github.com>
2017-12-08 20:14:55 +0900

    Update app.js

--- app.js
+++ app.js
@@ -56,11 +56,7 @@ client.on("message", async message => {
   if(command === "낮") {
 
     message.delete().catch(O_o=>{});
-    
-   message.channel.send("좋은아침입니다!", {
-    file: "https://cdn.discordapp.com/attachments/384356885970812928/385089929539223566/goodmoring0.5s.gif"
-   });
-               
+            
   }
   
   if(command === "아바타") {
