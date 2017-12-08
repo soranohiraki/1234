64cdadbbaff32f04867fb8676e31d34c6e25341e
sungpado <34066247+sungpado@users.noreply.github.com>
2017-12-08 16:27:31 +0900

    Update app.js

--- app.js
+++ app.js
@@ -132,7 +132,7 @@ message.channel.send("https://cdn.discordapp.com/attachments/384356885970812928/
   
   if(command === "킥") {
   
-    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
+    if(!message.member.roles.some(r=>["또라이", "멤버", "매니저", "ADMIN", "봇 개발자"].includes(r.name)) )
       return message.reply("죄송하지만 백청자여러분은 권한이 없습니다");
     
   
