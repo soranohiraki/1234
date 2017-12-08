8e7b311ca924c4324ff9940347f8202026cae9a6
sungpado <34066247+sungpado@users.noreply.github.com>
2017-12-08 15:55:05 +0900

    Update app.js

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ message.channel.send("https://cdn.discordapp.com/attachments/384356885970812928/
   
    }
   
-  if(command === "말") {
+  if(command === "") {
 
     const sayMessage = args.join(" ");
    
