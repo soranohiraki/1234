4bd6fb4ef7ea150b3e53f1295dc065fe800a9761
sungpado <34066247+sungpado@users.noreply.github.com>
2017-12-09 15:19:04 +0900

    Update app.js

--- app.js
+++ app.js
@@ -255,7 +255,7 @@ message.channel.send("백호님의 방송이 시작되었습니다. 공지방에
  
     const deleteCount = parseInt(args[0], 10);
     
-    if(!message.member.roles.some(r=>["또라이", "멤버", "매니저", "ADMIN", "봇 개발자"].includes(r.name)) )
+    if(!message.member.roles.some(r=>["또라이", "멤버", "TEAM", "매니저", "ADMIN", "봇 개발자"].includes(r.name)) )
       return message.reply("죄송하지만 백청자여러분은 권한이 없습니다");
     
     
