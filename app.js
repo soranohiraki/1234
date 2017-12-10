35ec49a6b75effe705a1e81e080603029addce39
sungpado <34066247+sungpado@users.noreply.github.com>
2017-12-10 18:06:19 +0900

    Update app.js

--- app.js
+++ app.js
@@ -40,14 +40,8 @@ client.on("message", async message => {
    
 
     let msg = await message.channel.send("투표");
-    
-    await msg.react(agree);
-    await msg.react(disagree)
-    
-    
-    const reactions = await msg.awaitReactions(reaction => reaction.emoji.name === agree || reaction.emoji.name === disagree, {time: 15000});
-    message.channel.send("투표결과 \n\n${agree}: ${reactions.get(agree).count-1}\n${disagree}: ${reactions.get(disagree).count-1}");
-  }
+    const config = require("vote.js");
+ }
   
   
   if(command === "겁먹지마") {
