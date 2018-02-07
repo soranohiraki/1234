dfcb775d8c1fc3e5928797984dcdefb3921d01d0
sungpado <34066247+sungpado@users.noreply.github.com>
2018-02-08 00:49:44 +0900

    Update app.js

--- app.js
+++ app.js
@@ -7,19 +7,9 @@ const embed = new Discord.RichEmbed()
 
 
 client.on("ready", () => {
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  client.user.setGame(`서버 관리중`);
+  client.user.setGame(`~도움말 을 쳐보세요.`);
 });
 
-client.on("guildCreate", guild => {
-  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setGame(`서버 관리중`);
-});
-
-client.on("guildDelete", guild => {
-  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setGame(`서버 관리중`);
-});
 
         
 
